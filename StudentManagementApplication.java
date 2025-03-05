d6baf0fbf665b491a658e6343a3f3a2edeb4321c
Fukuta Haruki <fukutaharuki@fukudaharukaonorenoMacBook-Air.local>
2025-03-05 19:12:35 +0900

    first comiit

---
+++ StudentManagementApplication.java
@@ -0,0 +1,28 @@
+package raisetech.StudentManagement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class StudentManagementApplication {
+
+    @Autowired
+    private StudentRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementApplication.class, args);
+    }
+
+    //nameとageをget
+    @GetMapping("/studentList")
+    public List<Student> getStudentList() {
+        return repository.search();
+    }
+
+
+}
